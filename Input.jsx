591a78dd8291877e80f4375b59e8111ff168ac9b
Matthijs van Henten <matthijs@c9.io>
2015-06-05 11:18:08 +0000

    reformat code after deamdfiy

--- Input.jsx
+++ Input.jsx
@@ -1,38 +1,38 @@
 
-    var React = require('react'),
-        FormField = require('./mixin/Field.jsx');
+var React = require('react'),
+    FormField = require('./mixin/Field.jsx');
 
-    /**
-     * Covers 90% use case for <input> element. Specify mutiline or rows in props
-     * to make a textarea.
-     * 
-     */
-    var Input = React.createClass({
-        displayName: 'Input',
+/**
+ * Covers 90% use case for <input> element. Specify mutiline or rows in props
+ * to make a textarea.
+ * 
+ */
+var Input = React.createClass({
+    displayName: 'Input',
 
-        mixins: [
-            FormField
-        ],
+    mixins: [
+        FormField
+    ],
 
-        renderInputElement: function() {
-            if (this.props.rows || this.props.multiLine)
-                return <textarea disabled={this.props.disabled} placeholder={this.props.placeholder} rows={this.props.rows || 3} className={this.classNames(this.props.className)} type={this.props.type} value={this.state.value} onChange={this.onChange} />;
-            return <input disabled={this.props.disabled} placeholder={this.props.placeholder} className={this.classNames(this.props.className)} type={this.props.type} value={this.state.value} onChange={this.onChange} />;
-        },
+    renderInputElement: function() {
+        if (this.props.rows || this.props.multiLine)
+            return <textarea disabled={this.props.disabled} placeholder={this.props.placeholder} rows={this.props.rows || 3} className={this.classNames(this.props.className)} type={this.props.type} value={this.state.value} onChange={this.onChange} />;
+        return <input disabled={this.props.disabled} placeholder={this.props.placeholder} className={this.classNames(this.props.className)} type={this.props.type} value={this.state.value} onChange={this.onChange} />;
+    },
 
-        renderLabel: function() {
-            if (this.props.label)
-                return <label>{this.props.label}</label>;
-        },
+    renderLabel: function() {
+        if (this.props.label)
+            return <label>{this.props.label}</label>;
+    },
 
-        render: function() {
-            return (
-                <div className={this.props.wrapperClassName}>
+    render: function() {
+        return (
+            <div className={this.props.wrapperClassName}>
                     {this.renderLabel()}
                     {this.renderInputElement()} {this.renderError()}
                   </div>
-                );
-        }
-    });
+            );
+    }
+});
 
-    module.exports = Input;
+module.exports = Input;
