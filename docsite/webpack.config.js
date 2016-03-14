990f16f9797d56f44751857dce4abd6624a56f4d
Matthijs van Henten <matthijs+foo@c9.io>
2016-03-14 00:02:04 +0000

    build to the root

--- docsite/webpack.config.js
+++ docsite/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 
     output: {
         filename: "index.js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "../"),
         /* IMPORTANT!
          * You must compile to UMD or CommonJS
          * so it can be required in a Node context: */
