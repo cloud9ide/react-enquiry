fbd41a78e044a2a31a5960c5fe64b06f5c986eb2
Matthijs van Henten <matthijs+foo@c9.io>
2016-10-16 22:41:37 +0000

    adds build script for site

---
+++ scripts/docsite.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+GIT_BRANCH=`git branch | grep -e "^*" | sed -e "s/\* //g"`
+GIT_COMMIT=`git log -n1 --pretty=oneline`
+
+echo "Generating new website for ${GIT_BRANCH}"
+cd docsite
+npm run build
+cd ..
+mv docsite/build .
+git checkout gh-pages
+cp build/* . -r
+rmdir build
+# git add -a
+# git commit -m"regenerated site from ${GIT_BRANCH\n${GIT_COMMIT}"
+git checkout $GIT_BRANCH
