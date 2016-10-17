49cacbb56c0516961a3c7b9bc64cb97bf8281374
Matthijs van Henten <matthijs+foo@c9.io>
2016-10-17 23:16:52 +0000

    ignore props that should not be transferred

--- fieldset.js
+++ fieldset.js
@@ -1,11 +1,19 @@
 "use strict";
 
 var React = require('react');
+var clone = require("lodash/clone");
 
 var transformChildren = require("./lib/transform-children");
 
 var Fieldset = React.createClass({
     render: function() {
+        var props = clone(this.props);
+
+        delete (props.defaultValues);
+
+        props.onSubmit = this.onSubmit;
+        props.noValidate = true;
+
         return React.createElement("fieldset", this.props, transformChildren(this.props.children, 0));
     },
 });
