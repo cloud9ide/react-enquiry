10cbb2067caf03b8e82ace6e5d38f63d65502154
Matthijs van Henten <matthijs+foo@c9.io>
2016-10-18 16:05:05 +0000

    update build doc file

--- build-docs.sh
+++ build-docs.sh
@@ -3,17 +3,15 @@
 GIT_BRANCH=`git branch | grep -e "^*" | sed -e "s/\* //g"`
 GIT_COMMIT=`git log -1 --pretty="format:%h %ai %s"`
 
-echo "Generating new website for ${GIT_BRANCH}"
-cd docsite
+echo "Generating new website for ${GIT_BRANCH} ${GIT_COMMIT}"
 npm install
 npm run build
-cd ..
 git clone . site-gen
 cd site-gen
-git checkout -t origin/gh-pages
+git checkout gh-pages
 cp ../docsite/build/* . -r
 git add .
 git commit -m "regenerated site ${GIT_BRANCH}: ${GIT_COMMIT}..."
 git push -u origin
-cd ..
-rm -rf site-gen
+# cd ..
+# rm -rf site-gen
